2967dd7712501217c0b2fd7a7fdded192cb9c103
Yifei Ma <yifeim@amazon.com>
2021-08-30 23:00:30 -0700

    tweak workflow

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     },
 
     install_requires=[
-        # "torch>=1.7.1+cu101", # cuda-specific
+        "torch>=1.7.1", # torch==1.7.1+cu101
         "pytorch-lightning>=1.3.8",
         "tick>=0.6",
         "lightfm>=1.16",
