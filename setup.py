26377d29d4cc21130dc0a86ff8d69c033b4eb749
Yifei Ma <yifeim@amazon.com>
2021-08-31 00:48:52 -0700

    hopeful

--- setup.py
+++ setup.py
@@ -11,9 +11,10 @@ setup(
         "rim_experiments": ["word_language_model/*.py"],
     },
 
-    install_requires=[
+    install_requires=[ # check out environment.yml for full list
         "torch>=1.7.1", # torch==1.7.1+cu101
         "pytorch-lightning>=1.3.8",
+        "numba>=0.52.0",
         # "tick>=0.6",
         "lightfm>=1.16",
         "pyarrow>=0.13.0",
