006f6c93eeaad12d5049f43268982e974f1f404f
Yifei Ma <yifeim@amazon.com>
2021-10-03 14:26:12 -0700

    update plot with improved numerical stability, not much helpful

--- src/rim_experiments/util/cvx_bisect.py
+++ src/rim_experiments/util/cvx_bisect.py
@@ -25,12 +25,13 @@ def primal_solution(u, v, s, eps):
     max_pi L(pi, u, v; ...) solved by
     pi = sigmoid[(s(x,y) - u(x) - v(y)) / eps]
     """
-    u = torch.as_tensor(u).to(s)
-    v = torch.as_tensor(v).to(s)
+    u = torch.as_tensor(u).to(s.device)
+    v = torch.as_tensor(v).to(s.device)
     r = s - u[:, None] - v[None, :]
     if eps > 0:
         return torch.sigmoid(r / eps)
     else:
+        # TODO: how to represent subgradients in [0, 1]?
         return torch.sign(r) * 0.5 + 0.5
 
 
@@ -39,8 +40,8 @@ def dual_complete(u, v, s, alpha, beta, eps):
     min_{u>=0, v<=0} d(u, v)
         = E_xy [ u(x)alpha(x) + v(y)beta(y) + Softplus(1/eps)(s-u-v) ]
     """
-    u = torch.as_tensor(u).to(s)
-    v = torch.as_tensor(v).to(s)
+    u = torch.as_tensor(u).to(s.device)
+    v = torch.as_tensor(v).to(s.device)
     if eps > 0:
         sp = torch.nn.Softplus(1./eps)(s - u[:, None] - v[None, :])
     else:
@@ -48,12 +49,31 @@ def dual_complete(u, v, s, alpha, beta, eps):
     return (u * alpha).mean() + (v * beta).mean() + sp.mean()
 
 
-def grad_u(u, v, s, alpha, eps):
+def grad_u(u, v, s, alpha, eps, stable=True):
     """
     find u = min{u>=0 : E_y[pi(x,y)] <= alpha(x)}
     """
-    pi = primal_solution(u, v, s, eps)
-    return alpha - pi.mean(axis=1)
+    if stable: # this does not make a practical difference wrt. slow convergence
+        u = torch.as_tensor(u).to(s.device)[:, None]
+        v = torch.as_tensor(v).to(s.device)[None, :]
+        alpha = torch.as_tensor(alpha)
+
+        z = alpha.log() - (1-alpha).log()
+        r = s - v
+        d = (u - r) / eps + z    # u = r - z * eps, if |Y|=1
+
+        d_lt_0 = ( d.exp() - 1 ) / (
+            (z.exp() + 1) * (((u - r) / eps).exp() + 1)
+        ) # grad < 0
+
+        d_gt_0 = ( 1 - (-d).exp() ) / (
+            ((-z).exp() + 1) * (((r - u) / eps).exp() + 1)
+        ) # grad > 0
+
+        return torch.where(d < 0, d_lt_0, d_gt_0).mean(axis=1)
+    else:
+        pi = primal_solution(u, v, s, eps)
+        return alpha - pi.mean(axis=1)
 
 
 def dual_solve_u(v, s, alpha, eps, verbose=False, approx_quantiles=True):
@@ -66,9 +86,9 @@ def dual_solve_u(v, s, alpha, eps, verbose=False, approx_quantiles=True):
     if alpha < 0 or alpha > 1:
         warnings.warn(f"clipping alpha={alpha} to [0, 1]")
         alpha = np.clip(alpha, 0, 1)
-    v = torch.as_tensor(v).to(s)
-    alpha = torch.as_tensor(alpha).to(s)
-    eps = torch.as_tensor(eps).to(s)
+    v = torch.as_tensor(v).to(s.device)
+    alpha = torch.as_tensor(alpha).to(s.device)
+    eps = torch.as_tensor(eps).to(s.device)
 
     # z = (s-u-v)/eps inf * 0 = inf
     z = alpha.log() - (1-alpha).log()
@@ -159,32 +179,50 @@ if __name__ == '__main__':
     matplotlib.rcParams['ps.fonttype'] = 42
 
     s = torch.tensor([[1], [0.5]])
-    alpha = 1.0
+    alpha = 0.99
     beta = 0.4
     eps = 1
 
-    fig, ax = pl.subplots(figsize=(5,4))
-    v_list = np.linspace(-1,3, 100)
+    fig, ax = pl.subplots(figsize=(4,3))
+    v_list = np.linspace(-1, 3.8, 100)
     colors = []
-    for i, eps in enumerate([1, 0.5, 0]):
+    for i, eps in enumerate([1, 0.5, 0.01]):
         f = [dual(torch.as_tensor([v]), s, alpha, beta, eps).tolist()
              for v in v_list]
-        p = pl.plot(v_list, f, ls=':', label=f"$\epsilon$={eps}")
+        p = pl.plot(v_list, f, ls=':', label=f'$\epsilon$={eps}')
         colors.append(p[0].get_color())
 
-    for i, eps in enumerate([1, 0.5, 0]):
-        pl.plot(*zip(*[(v.cpu().numpy(), y.cpu().numpy())
-          for v, y in dual_iterate(
-              torch.as_tensor([-0.5]), s, alpha, beta, eps,
-          )]), 'o--', mfc='none', color=colors[i], label=f'alternating' if i==0 else None)
-
-    for i, eps in enumerate([1, 0.5, 0]):
+    for i, eps in enumerate([1, 0.5, 0.01]):
         pl.plot(*zip(*[(v.cpu().numpy(), y.cpu().numpy())
           for v, y in dual_iterate(
               torch.as_tensor([-0.5]), s, alpha, beta, eps, stepsize=2,
-          )]), '+-', color=colors[i], label=f'subgradient' if i==0 else None)
+          )]), '+-', color=colors[i], label=f'(sub)gradient' if i==2 else None)
 
-    pl.ylabel("dual objective", fontsize=14)
-    pl.xlabel("dual variable v", fontsize=14)
+    for i, eps in enumerate([1, 0.5, 0.01]):
+        pl.plot(*zip(*[(v.cpu().numpy(), y.cpu().numpy())
+          for v, y in dual_iterate(
+              torch.as_tensor([-0.5]), s, alpha, beta, eps,
+          )]), 'o--', mfc='none', color=colors[i], lw=2,
+                label=f'alternating' if i==2 else None)
+
+    v = torch.as_tensor([-0.5])
+    arr = []
+    for eps in [1, 0.5, 0.01]:
+        for _ in range(10):
+            u = dual_solve_u(v, s, alpha, eps)
+            u = dual_clip(u, 'ub')
+            f = dual_complete(u, v, s, alpha, beta, eps)
+            arr.append([v.numpy(), f.numpy()])
+
+            v = dual_solve_u(u, s.T, beta, eps)
+
+            u = dual_solve_u(v, s, alpha, eps)
+            u = dual_clip(u, 'ub')
+            f = dual_complete(u, v, s, alpha, beta, eps)
+            arr.append([v.numpy(), f.numpy()])
+    pl.plot(*zip(*arr), '.--', label='annealed')
+
+    pl.ylabel("dual objective", fontsize=12)
+    pl.xlabel("dual variable v", fontsize=12)
     pl.legend(loc='upper right')
     fig.savefig('cvx_synthetic.pdf', bbox_inches='tight')
