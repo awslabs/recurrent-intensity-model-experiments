fa9ac5417bfb19701f82cd10cfa2b5a39dcbf9ee
yifeim <yma@yma.io>
2021-11-06 16:46:49 -0700

    shuffle codes

--- src/rime/dataset/__init__.py
+++ src/rime/dataset/__init__.py
@@ -7,31 +7,6 @@ from .prepare_ml_1m_data import prepare_ml_1m_data
 from .prepare_yoochoose_data import prepare_yoochoose_data
 
 
-def prepare_synthetic_data(split_fn_name, exclude_train=False,
-    num_users=300, num_items=200, num_events=10000):
-    """ prepare synthetic data for end-to-end unit tests """
-    event_df = pd.DataFrame({
-        'USER_ID': np.random.choice(num_users, num_events),
-        'ITEM_ID': np.random.choice(num_items, num_events)+1, # pandas bug
-        'TIMESTAMP': np.random.uniform(0, 5, num_events),
-    }).sort_values(["USER_ID", "TIMESTAMP"])
-
-    user_df, item_df = extract_user_item(event_df)
-
-    if split_fn_name == 'split_by_time':
-        user_df, valid_df = split_by_time(user_df, 4, 3)
-    elif split_fn_name == 'split_by_user':
-        user_df, valid_df = split_by_user(user_df, user_df.index % 2, 3)
-    else:
-        raise ValueError(f"unknown {split_fn_name}")
-
-    D = create_dataset(event_df, user_df, item_df, 1, exclude_train=exclude_train)
-    D._is_synthetic_data = True # for hawkes_poisson verification purposes
-    D.print_stats()
-    V = create_dataset(event_df, valid_df, item_df, 1, exclude_train=exclude_train)
-    return (D, V)
-
-
 def prepare_minimal_dataset():
     """ minimal dataset to enable main workflow in unit tests """
     T_split = 10
@@ -75,3 +50,28 @@ def prepare_minimal_dataset():
     )
     D.print_stats()
     return (D, None)
+
+
+def prepare_synthetic_data(split_fn_name, exclude_train=False,
+    num_users=300, num_items=200, num_events=10000):
+    """ prepare synthetic data for end-to-end unit tests """
+    event_df = pd.DataFrame({
+        'USER_ID': np.random.choice(num_users, num_events),
+        'ITEM_ID': np.random.choice(num_items, num_events)+1, # pandas bug
+        'TIMESTAMP': np.random.uniform(0, 5, num_events),
+    }).sort_values(["USER_ID", "TIMESTAMP"])
+
+    user_df, item_df = extract_user_item(event_df)
+
+    if split_fn_name == 'split_by_time':
+        user_df, valid_df = split_by_time(user_df, 4, 3)
+    elif split_fn_name == 'split_by_user':
+        user_df, valid_df = split_by_user(user_df, user_df.index % 2, 3)
+    else:
+        raise ValueError(f"unknown {split_fn_name}")
+
+    D = create_dataset(event_df, user_df, item_df, 1, exclude_train=exclude_train)
+    D._is_synthetic_data = True # for hawkes_poisson verification purposes
+    D.print_stats()
+    V = create_dataset(event_df, valid_df, item_df, 1, exclude_train=exclude_train)
+    return (D, V)
